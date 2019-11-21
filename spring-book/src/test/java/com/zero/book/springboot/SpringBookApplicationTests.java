134dd01ff946952477f6d7dd1fde97cd2b550796
JHackermask <337042505@qq.com>
2019-11-21 20:06:49 +0800

    init

---
+++ spring-book/src/test/java/com/zero/book/springboot/SpringBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.book.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
