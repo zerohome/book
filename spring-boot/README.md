df74c48fa5eeb8a025c2d9e1a32d72a876b17620
JHackermask <337042505@qq.com>
2019-11-21 23:05:40 +0800

    Embedded Server

--- spring-boot/README.md
+++ spring-boot/README.md
@@ -10,3 +10,10 @@ Build-Jdk-Spec: 1.8
 Spring-Boot-Version: 2.2.1.RELEASE
 Created-By: Maven Archiver 3.4.0
 Main-Class: org.springframework.boot.loader.JarLauncher
+
+## (Embedded Server)嵌入式容器
+Tomcat
+Jetty
+Undertow
+
+## (Auto Configuration) 自动装配
--- spring-boot/src/main/java/com/zero/book/springboot/Application.java
+++ spring-boot/src/main/java/com/zero/book/springboot/Application.java
@@ -1,7 +1,11 @@
 package com.zero.book.springboot;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +22,23 @@ public class Application {
 		return "Hello World";
 	}
 
+	/**
+	 * <p>ApplicationRunner#run(ApplicationArguments) 会在应用启动后回调 .</p>
+	 * <p>时间 : 2019年11月21日</p>
+	 * <p>Copyright 2017-2017, HackMask Tech. Co., Ltd.</p>
+	 * @author  zhangjj25968
+	 * @version 1.0
+	 * @see Application#onWebServerReady(WebServerInitializedEvent)
+	 * @return
+	 */
+	@Bean
+	@Deprecated
+	public ApplicationRunner runner() {
+		return args -> {};
+	}
+	
+	@EventListener(WebServerInitializedEvent.class)
+	public void onWebServerReady(WebServerInitializedEvent initEvent) {
+		initEvent.getWebServer();
+	}
 }
