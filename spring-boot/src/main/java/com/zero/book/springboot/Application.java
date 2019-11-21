bbf94cd94e85c5363ddc69a9eebdff0e3cf1582e
JHackermask <337042505@qq.com>
2019-11-21 22:04:22 +0800

    web

--- spring-boot/src/main/java/com/zero/book/springboot/Application.java
+++ spring-boot/src/main/java/com/zero/book/springboot/Application.java
@@ -2,12 +2,20 @@ package com.zero.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello World";
 	}
 
 }
