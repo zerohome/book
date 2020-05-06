1f23eeebded49a8b048b144c0f32eb06a2a42337
JHackermask <337042505@qq.com>
2020-05-06 09:34:00 +0800

    常量池

---
+++ turing-demo/src/main/java/xyz/familyz/turing/CommonPool.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright(c) by 2017-2017 HackMask Tefchnologies , Ltd. All Rights Reserved
+ *
+ */
+package xyz.familyz.turing;
+
+/**
+ * <p>TODO .</p>
+ * <p>时间 ： 2020年4月23日</p>
+ * <p>Copyright 2017-2017, HackMask Tech. Co., Ltd.</p>
+ * @author  1150310037
+ * @version 1.0
+ */
+public class CommonPool {
+
+	public static final String QUIT = "QUIT";
+
+	public static final int PORT = 8888;
+	
+	public static final int CAPACITY = 10;
+	
+	public static final String IP = "127.0.0.1";
+	
+	public static final String KAFKA_SERVERS = "111.229.3.73:9092";
+	
+	
+}
